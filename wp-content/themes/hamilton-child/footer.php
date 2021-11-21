3eb9d21f2ba6d50358399be936a706e84c16686e
monicaalv <moni5966@stud.kea.dk>
2021-11-21 20:18:08 +0100

    ferdig (uten footer)

--- wp-content/themes/hamilton-child/footer.php
+++ wp-content/themes/hamilton-child/footer.php
@@ -1,9 +1,14 @@
 		</main>
 		
 		<footer class="site-footer section-inner">
+
+<h2>Kontakt os her:</h2>
+<p>strikkekollektivet@strik.dk </p> <br>
+<p>Tlf: 0045 99999991</p> <br>
+
 			
 
-			<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>" class="site-name"><?php bloginfo( 'name' ); ?></a></p>
+			<br><p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>" class="site-name"><?php bloginfo( 'name' ); ?></a></p>
 			<p class="theme-by"><?php _e( '', '' ); ?> </p>
 
 		</footer><!-- footer -->
